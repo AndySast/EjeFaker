a84f13b16bd6ac6e144361d978edbc0995beac96
AndySast <andressastoquexyz@gmail.com>
2022-05-15 19:53:35 -0500

    tryCatch

--- faker/ejercicio.js
+++ faker/ejercicio.js
@@ -1,10 +1,12 @@
-const fs = require('fs')
-const { faker } = require('@faker-js/faker');
-const { Console } = require('console');
-names = ''
-for(let i = 0;i<1000;i++){
-    names += (faker.name.findName()) + '\n'
-    }
-fs.writeFileSync('names.txt', names)
-console.log('file names has been create')
-
+const fs = require("fs");
+const { faker } = require("@faker-js/faker");
+names = "";
+for (let i = 0; i < 1000; i++) {
+  names += faker.name.findName() + "\n";
+}
+try {
+  fs.writeFileSync("names.txt", names);
+  console.log("file names has been create");
+} catch (err) {
+  console.log("We had an error while the it happened the process");
+}
